f0bad0342c7588ac20c74722d84ba7b07cb79dd0
Isao Yagi <isao@yahoo-inc.com>
2012-09-11 14:24:02 -0700

    go back to function declaration style

--- templates/helpers.js
+++ templates/helpers.js
@@ -5,39 +5,39 @@ function shortSha(sha) {
     return sha.trim().replace(/^[a-f0-9]{40}$/, sha.slice(0, shortShaLen));
 }
 
-module.exports = {
+function range2version(str) {
+    return str.split('..')[1];
+}
 
-    init: function() {
-        // set some internal state here if needed
-    },
+function compareView(str) {
+    var range = str.split('..');
+    return '/yahoo/mojito/compare/' + range[0] + '...' + range[1];
+}
 
-    range2version: function(str) {
-        return str.split('..')[1];
-    },
+function unwrap(str) {
+    var words = str.split(/\s+/gm),
+        out = '';
 
-    compareView: function(str) {
-        var range = str.split('..');
-        return '/yahoo/mojito/compare/' + range[0] + '...' + range[1];
-    },
-
-    unwrap: function(str) {
-        var words = str.split(/\s+/gm), out = '';
-
-        for(; words.length; out += ' ' + words.shift()) {
-            if(out.length > unwrapMax) {
-                out += '…';
-                break;
-            }
+    for(; words.length; out += ' ' + words.shift()) {
+        if(out.length > unwrapMax) {
+            out += '…';
+            break;
         }
-        return out;
-    },
-
-    shortSha: shortSha,
-
-    commitLink: function(sha) {
-        var commit = shortSha(sha),
-            href = '/yahoo/mojito/commit/';
-
-        return ['[', commit, '](', href, commit, ')'].join('');
     }
+    return out;
+}
+
+function commitLink(sha) {
+    var commit = shortSha(sha),
+        href = '/yahoo/mojito/commit/';
+
+    return ['[', commit, '](', href, commit, ')'].join('');
+}
+
+module.exports = {
+    range2version: range2version,
+    compareView: compareView,
+    unwrap: unwrap,
+    shortSha: shortSha,
+    commitLink: commitLink
 };
